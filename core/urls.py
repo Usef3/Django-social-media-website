8c9b3c8755d640cc66c941de25f3d05d3c1b57de
YousefElsheikh2003 <134155685+YousefElsheikh2003@users.noreply.github.com>
2024-05-25 01:53:19 +0300

    edit

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
-    path('',views.index,name='home')
+    path("", views.index, name="home"),
+    path("signup/", views.signup, name="signup"),
 ]
