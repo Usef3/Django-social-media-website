f407336651d51eba0e4907a6a3aece652480e227
YousefElsheikh2003 <134155685+YousefElsheikh2003@users.noreply.github.com>
2024-05-28 09:20:41 +0300

    update

--- core/views.py
+++ core/views.py
@@ -1,34 +1,73 @@
-from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth.models import User,auth
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url="signin")
 def index(request):
-    return render(request,'index.html')
+    return render(request, "index.html")
+
+
 def signup(request):
-    if request.method=='POST':
+    if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
         password = request.POST["password"]
         password2 = request.POST["password2"]
-        if password==password2:
+        if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request,'Email Taken')
+                messages.info(request, "Email Taken")
                 return redirect("signup")
             elif User.objects.filter(username=username).exists():
-                messages.info(request,'Username Taken')
+                messages.info(request, "Username Taken")
                 return redirect("signup")
             else:
-                user=User.objects.create_user(username=username,email=email,password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
                 user.save()
-                user_model=User.objects.get(username=username)
-                new_profile=Profile.objects.create(user=user_model,id_user=user_model.id)
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id
+                )
                 new_profile.save()
-                return redirect('signup')
+                return redirect("signup")
         else:
             messages.info(request, "password not matching")
             return redirect("signup")
 
     else:
         return render(request, "signup.html")
+
+
+def signin(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("home")
+        else:
+            messages.info(request, "Credentials Invalid")
+            return redirect("signin")
+
+    else:
+        return render(request, "signin.html")
+
+
+@login_required(login_url="signin")
+def logout(request):
+    auth.logout(request)
+    return redirect("signin")
+
+
+@login_required(login_url="signin")
+def settings(request):
+    return render(request, "setting.html")
