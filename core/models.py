b9170f75adabfdd7c2a9c8aeef21d3303e04e69a
YousefElsheikh2003 <134155685+YousefElsheikh2003@users.noreply.github.com>
2024-06-11 18:35:13 +0300

    updatee

--- core/models.py
+++ core/models.py
@@ -6,12 +6,13 @@ from datetime import datetime
 User = get_user_model()
 
 
+# Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(
-        upload_to="profileimages", default="blank-profile-picture.png"
+        upload_to="profile_images", default="blank-profile-picture.png"
     )
     location = models.CharField(max_length=100, blank=True)
 
@@ -20,13 +21,28 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    id=models.UUIDField(primary_key=True,default=uuid.uuid4)
-    user=models.CharField(max_length=100)
-    image=models.ImageField( upload_to='post_images')
-    caption=models.TextField()
-    created_at=models.TimeField(default=datetime.now)
-    no_of_likes=models.IntegerField(default=0)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="post_images")
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
 
-    
-    def __str__(self) :
+    def __str__(self):
+        return self.user
+
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
         return self.user
